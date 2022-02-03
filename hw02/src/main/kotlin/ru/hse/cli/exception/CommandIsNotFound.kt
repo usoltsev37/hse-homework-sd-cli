fefbba1908afc5c51e8e48487c336ac43831bf23
smirok <smirok123@mail.ru>
2022-02-04 01:58:26 +0300

    Update package hierarchy in test directory & delete CommandIsNotFoundException

--- hw02/src/main/kotlin/ru/hse/cli/exception/CommandIsNotFound.kt
+++
@@ -1,6 +0,0 @@
-package ru.hse.cli.exception
-
-/**
- * The CommandIsNotFoundException is raised in case of access to a nonexistent command in CommandStorage.
- */
-open class CommandIsNotFoundException(message: String) : Exception(message)
