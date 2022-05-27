b022441adb36e16ed05c1d1038f7990d2d65864c
Readrid <890readrid@gmail.com>
2022-05-27 15:33:09 +0400

    Add docs for exceptions

--- hw02/src/main/kotlin/ru/hse/cli/exception/UnknownCommandException.kt
+++ hw02/src/main/kotlin/ru/hse/cli/exception/UnknownCommandException.kt
@@ -4,4 +4,4 @@ package ru.hse.cli.exception
  * Exception will be thrown if [ProcessBuilder] cannot start process with the particular command name
  * @param name is name of command
  */
-class UnknownCommandException(val name: String): Exception()
+class UnknownCommandException(name: String): Exception(name)
